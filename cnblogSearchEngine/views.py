40813d1f6af2e74e8847957f9894f8638c702c70
Your Name <you@example.com>
2020-03-24 09:36:38 +0800

    Initial commit

---
+++ cnblogSearchEngine/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def dosearch(request):
+    keyword = request.GET.get('q','')
+    print(keyword)
+    return render(request,'results.html')
